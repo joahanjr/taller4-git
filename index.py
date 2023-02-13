b0336c942c22f1ee72ddd1cd273e8d46348d46d9
joahan <joahanramirez7@gmail.com>
2023-02-12 23:24:25 -0600

    final

--- index.py
+++ index.py
@@ -1,26 +1,30 @@
 from flask import Flask, render_template
+
 app = Flask(__name__)
 
 @app.route('/')
 def index_():
-    return render_template('index.py')
+    return render_template('index.html')
 
-@app.route('/index.html')
+@app.route ('/index.html')
 def index():
     return render_template('index.html')
 
-@app.route('/servicios.html')
+@app.route ('/servivios.html')
 def servicios():
-    return render_template( 'servicios.html')
+    return render_template('servicios.html')
 
-@app.route('/especialidades.html')
+@app.route ('/especialidades.html')
 def especialidades():
     return render_template ('especialidades.html')
 
-@app.route('/quienes_somos.html')
-def quienes_somos():
-    return render_template ('quienes_somos.html')
+@app.route ('/quienesSomos.html')
+def quienesSomos():
+    return render_template ('quienesSomos.html')
 
 @app.route ('/contacto.html')
 def contacto():
     return render_template ('contacto.html')
+
+if __name__ == '__main__':
+    app.run(debug=True, port=4000)
--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -30,7 +30,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/joahanjr/escuela/taller4/taller4-git/pag/venv"
+set -gx VIRTUAL_ENV "/home/joahanjr/escuela/taller4/taller4-git/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
